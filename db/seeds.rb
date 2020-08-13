21ce50e7270faaf93d66cbe176980761d4f9d140
victor bergeaud <victor.bergeaud@gmail.com>
2020-08-13 17:07:08 +0200

    Mister Cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredient = JSON.parse(open(url).read)
+ingredient['drinks'].each do |i|
+  name = i['strIngredient1']
+  Ingredient.create(name: name)
+end
